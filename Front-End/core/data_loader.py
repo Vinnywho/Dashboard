c9d4964152763ef2092b3d9af0e00b5fbec19220
Vinicius Lima <vivini13235@gmail.com>
2026-05-11 22:14:50 -0300

    .

--- Front-End/core/data_loader.py
+++ Front-End/core/data_loader.py
@@ -1,8 +1,23 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
+import os
 
 @st.cache_data
 def carregar_dados():
+    diretorio_atual = os.path.dirname(os.path.abspath(__file__))
+    
+    def get_path(nome_arquivo):
+        caminhos = [
+            os.path.join(diretorio_atual, "..", "data", nome_arquivo),
+            os.path.join(os.getcwd(), "data", nome_arquivo),
+            os.path.join(os.getcwd(), "Front-End", "data", nome_arquivo),
+            os.path.join(diretorio_atual, "data", nome_arquivo)
+        ]
+        for c in caminhos:
+            if os.path.exists(c):
+                return c
+        return nome_arquivo
+
     df_campanhas = pd.read_csv('../data/CAMPAIGN.CSV')
     df_conversoes = pd.read_csv('../data/CAMPAIGNxORDER.CSV')
     df_pedidos_loja = pd.read_csv('../data/STOREORDER.csv')
@@ -10,12 +25,11 @@ def carregar_dados():
     df_clientes = pd.read_csv('../data/CUSTOMER.CSV')
     df_enderecos = pd.read_csv('../data/CUSTOMERADDRESS.CSV')
 
-    mapa_lojas = dict(zip(df_stores['id'], df_stores['name']))
-
-    for df, col in [(df_campanhas, 'sendat'), (df_pedidos_loja, 'createdat'), (df_clientes, 'createdat')]:
-        df[col] = pd.to_datetime(df[col], errors='coerce', utc=True)
-    
-    df_pedidos_loja['scheduledat'] = pd.to_datetime(df_pedidos_loja['scheduledat'], format='mixed')
+    df_campanhas['sendat'] = pd.to_datetime(df_campanhas['sendat'], errors='coerce')
+    df_conversoes['sent_at'] = pd.to_datetime(df_conversoes['sent_at'], errors='coerce')
+    df_pedidos_loja['createdat'] = pd.to_datetime(df_pedidos_loja['createdat'], errors='coerce')
     df_clientes['dateofbirth'] = pd.to_datetime(df_clientes['dateofbirth'], errors='coerce')
+
+    mapa_lojas = dict(zip(df_stores['id'], df_stores['name']))
 
     return df_campanhas, df_conversoes, df_pedidos_loja, mapa_lojas, df_clientes, df_enderecos
