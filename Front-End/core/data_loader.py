b158b94af39680d89ecf01b683f4b10278d2a253
Vinicius Lima <vivini13235@gmail.com>
2026-05-11 22:18:41 -0300

    Teste de deploy nº3

--- Front-End/core/data_loader.py
+++ Front-End/core/data_loader.py
@@ -4,26 +4,27 @@ import os
 
 @st.cache_data
 def carregar_dados():
-    diretorio_atual = os.path.dirname(os.path.abspath(__file__))
+    diretorio_core = os.path.dirname(os.path.abspath(__file__))
+    raiz_projeto = os.path.dirname(os.path.dirname(diretorio_core))
     
     def get_path(nome_arquivo):
         caminhos = [
-            os.path.join(diretorio_atual, "..", "data", nome_arquivo),
+            os.path.join(raiz_projeto, "data", nome_arquivo),
+            os.path.join(os.path.dirname(diretorio_core), "data", nome_arquivo),
             os.path.join(os.getcwd(), "data", nome_arquivo),
-            os.path.join(os.getcwd(), "Front-End", "data", nome_arquivo),
-            os.path.join(diretorio_atual, "data", nome_arquivo)
+            os.path.join(os.getcwd(), "Front-End", "data", nome_arquivo)
         ]
         for c in caminhos:
             if os.path.exists(c):
                 return c
-        return nome_arquivo
+        return os.path.join(raiz_projeto, "data", nome_arquivo)
 
-    df_campanhas = pd.read_csv('../data/CAMPAIGN.CSV')
-    df_conversoes = pd.read_csv('../data/CAMPAIGNxORDER.CSV')
-    df_pedidos_loja = pd.read_csv('../data/STOREORDER.csv')
-    df_stores = pd.read_csv('../data/STORE.CSV')
-    df_clientes = pd.read_csv('../data/CUSTOMER.CSV')
-    df_enderecos = pd.read_csv('../data/CUSTOMERADDRESS.CSV')
+    df_campanhas = pd.read_csv(get_path('CAMPAIGN.CSV'))
+    df_conversoes = pd.read_csv(get_path('CAMPAIGNxORDER.CSV'))
+    df_pedidos_loja = pd.read_csv(get_path('STOREORDER.csv'))
+    df_stores = pd.read_csv(get_path('STORE.CSV'))
+    df_clientes = pd.read_csv(get_path('CUSTOMER.CSV'))
+    df_enderecos = pd.read_csv(get_path('CUSTOMERADDRESS.CSV'))
 
     df_campanhas['sendat'] = pd.to_datetime(df_campanhas['sendat'], errors='coerce')
     df_conversoes['sent_at'] = pd.to_datetime(df_conversoes['sent_at'], errors='coerce')
